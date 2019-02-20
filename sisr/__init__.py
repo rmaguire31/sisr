5c8ff42c13e0d20494bdd9f38e4f1c2540f34e7e
Russell Maguire <rmaguire31@gmail.com>
2019-02-20 12:37:04 +0000

    Upgrade version number

--- sisr/__init__.py
+++ sisr/__init__.py
@@ -1,6 +1,6 @@
 """SiSR PyTorch Network for super-resolution in semiconductor device inspection
 """
-__version_info__ = '0', '1', '1', 'dev0',
+__version_info__ = '0', '2', '0',
 __version__ = '.'.join(__version_info__)
 
 
