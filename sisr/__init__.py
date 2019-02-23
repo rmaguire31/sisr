47ab7bc47d200ff585f3c5d4046c7b03e6d3209b
rmaguire31 <rmaguire31@gmail.com>
2019-02-23 10:37:17 +0000

    Incremented patch version

--- sisr/__init__.py
+++ sisr/__init__.py
@@ -1,6 +1,6 @@
 """SiSR PyTorch Network for super-resolution in semiconductor device inspection
 """
-__version_info__ = '0', '2', '2',
+__version_info__ = '0', '2', '3',
 __version__ = '.'.join(__version_info__)
 
 
