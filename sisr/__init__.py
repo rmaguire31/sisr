d7c27ec60f0f3e55d8142202874a880dcd3bc2b6
rmaguire31 <rmaguire31@gmail.com>
2019-02-25 13:18:37 +0000

    Updated patch version

--- sisr/__init__.py
+++ sisr/__init__.py
@@ -1,6 +1,6 @@
 """SiSR PyTorch Network for super-resolution in semiconductor device inspection
 """
-__version_info__ = '0', '2', '5',
+__version_info__ = '0', '2', '6',
 __version__ = '.'.join(__version_info__)
 
 
